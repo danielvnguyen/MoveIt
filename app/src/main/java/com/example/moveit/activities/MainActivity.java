310546272f7c409550929fec9ab600976729b2b5
Daniel Nguyen <danielnguyen604@gmail.com>
2022-09-30 20:31:18 -0700

    create model & view packages

--- app/src/main/java/com/example/moveit/activities/MainActivity.java
+++ app/src/main/java/com/example/moveit/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.moveit;
+package com.example.moveit.activities;
 
 import android.os.Bundle;
 
+import com.example.moveit.R;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
