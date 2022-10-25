b83a488d8689c57770ceff6a2a721dbb567a63a5
Daniel Nguyen <danielnguyen604@gmail.com>
2022-10-24 21:10:55 -0700

    add moods to addEntry

--- app/src/main/java/com/example/moveit/activities/AddEntry.java
+++ app/src/main/java/com/example/moveit/activities/AddEntry.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.moveit.R;
+import com.example.moveit.model.Entry;
 
 import java.util.Objects;
 
 public class AddEntry extends AppCompatActivity {
+
+    private Button saveBtn;
+    private ImageView green;
+    private ImageView yellow;
+    private ImageView red;
+    private Entry currentEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_add_entry);
+
+        //setUpMoods();
+        //setUpSaveBtn();
     }
 
     @Override
