53931a71e227b9ef387b234f9379e0851bf9917a
Daniel Nguyen <danielnguyen604@gmail.com>
2022-10-26 21:50:00 -0700

    setup mealList

--- app/src/main/java/com/example/moveit/ui/meals/MealList.java
+++ app/src/main/java/com/example/moveit/ui/meals/MealList.java
@@ -6,17 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.moveit.R;
-import com.example.moveit.ui.entries.AddEntry;
-import com.example.moveit.ui.entries.EntriesList;
+import com.example.moveit.model.Meal;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MealList extends AppCompatActivity {
 
     private FloatingActionButton addMealBtn;
+    private ArrayAdapter<Meal> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,23 @@ public class MealList extends AppCompatActivity {
 
         addMealBtn = findViewById(R.id.addMeal);
 
+        setUpMealList();
         setUpAddMealBtn();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        listAdapter.notifyDataSetChanged();
+    }
+
+    private void setUpMealList() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert currentUser != null;
+
+        ListView mealListView = findViewById(R.id.mealList);
+        ArrayList<Meal> mealList = new ArrayList<>();
+        listAdapter = new ArrayAdapter<>(this, R.layout.meal_list_item, mealList);
+        mealListView.setAdapter(listAdapter);
     }
 
     private void setUpAddMealBtn() {
