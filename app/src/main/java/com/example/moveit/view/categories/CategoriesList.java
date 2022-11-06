a4891a72a04d9b45547ab0647078725e4e98c0f8
Daniel Nguyen <danielnguyen604@gmail.com>
2022-11-05 17:06:13 -0700

    define application colours

--- app/src/main/java/com/example/moveit/view/categories/CategoriesList.java
+++ app/src/main/java/com/example/moveit/view/categories/CategoriesList.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.moveit.R;
+
 public class CategoriesList extends AppCompatActivity {
 
     @Override
