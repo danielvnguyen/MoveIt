2990dc8171609006af898e7f3ef644df0afce307
Daniel Nguyen <danielnguyen604@gmail.com>
2022-11-09 20:54:32 -0800

    add icons and button borders

--- app/src/main/java/com/example/moveit/view/activities/AddActivity.java
+++ app/src/main/java/com/example/moveit/view/activities/AddActivity.java
@@ -11,6 +11,6 @@ public class AddActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.activity_add_activity);
     }
 }
