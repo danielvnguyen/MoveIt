f9486d6bb92aa51ac434926fe8fb19996952732e
Daniel Nguyen <danielnguyen604@gmail.com>
2022-11-12 01:46:06 -0800

    implement change theme functionality

--- app/src/main/java/com/example/moveit/view/SetThemeActivity.java
+++ app/src/main/java/com/example/moveit/view/SetThemeActivity.java
@@ -1,8 +1,10 @@
 package com.example.moveit.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatDelegate;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.moveit.R;
 
@@ -13,5 +15,43 @@ public class SetThemeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_theme);
         setTitle("Change Theme");
+
+        setUpButtons();
+    }
+
+    private void setUpButtons() {
+        Button lightBtn = findViewById(R.id.lightBtn);
+        Button darkBtn = findViewById(R.id.darkBtn);
+        Button systemDefBtn = findViewById(R.id.systemDefaultBtn);
+
+        lightBtn.setOnClickListener(v -> {
+            int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            switch (currentNightMode) {
+                case Configuration.UI_MODE_NIGHT_NO:
+                    break;
+                case Configuration.UI_MODE_NIGHT_YES:
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    break;
+                default:
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+            }
+        });
+
+        darkBtn.setOnClickListener(v -> {
+            int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            switch (currentNightMode) {
+                case Configuration.UI_MODE_NIGHT_NO:
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    break;
+                case Configuration.UI_MODE_NIGHT_YES:
+                    break;
+                default:
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+            }
+        });
+
+        systemDefBtn.setOnClickListener(v -> {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        });
     }
 }
