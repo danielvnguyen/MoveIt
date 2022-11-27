97148e0dd903466a30213bb6dcb2f07802a120f1
Daniel Nguyen <danielnguyen604@gmail.com>
2022-11-27 00:10:47 -0800

    implement base download image func.

--- app/src/main/java/com/example/moveit/view/photoGallery/ViewPhotoActivity.java
+++ app/src/main/java/com/example/moveit/view/photoGallery/ViewPhotoActivity.java
@@ -1,10 +1,24 @@
 package com.example.moveit.view.photoGallery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,6 +32,10 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -25,6 +43,8 @@ import java.util.Objects;
 public class ViewPhotoActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
     private String currentImageId;
+    private ImageView currentImageView;
+    private static final Integer REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,13 +94,66 @@ public class ViewPhotoActivity extends AppCompatActivity {
             final StorageReference fileRef = FirebaseStorage.getInstance().getReference().child(currentUser.getUid())
                     .child("uploads").child(currentImageId);
             fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
-
+                if (ContextCompat.checkSelfPermission(ViewPhotoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    saveImage();
+                } else {
+                    ActivityCompat.requestPermissions(ViewPhotoActivity.this, new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+                }
             });
         });
     }
 
+    private void saveImage() {
+        Uri images;
+        ContentResolver contentResolver = getContentResolver();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            images = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+        } else {
+            images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        String imageTitle = System.currentTimeMillis() + ".jpg";
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imageTitle);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "images/*");
+        Uri uri = contentResolver.insert(images, contentValues);
+        try {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) currentImageView.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+
+            OutputStream outputStream = contentResolver.openOutputStream(uri);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            Toast.makeText(ViewPhotoActivity.this, "Imaged saved to phone successfully",
+                    Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(ViewPhotoActivity.this, "Failed to save image to phone",
+                    Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveImage();
+            } else {
+                Toast.makeText(ViewPhotoActivity.this, "Please provide required permission to download images",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     private void setUpInterface() {
-        ImageView currentImageView = findViewById(R.id.currentImage);
+        currentImageView = findViewById(R.id.currentImage);
         TextView dateTimeTV = findViewById(R.id.dateTimeTV);
 
         Bundle extras = getIntent().getExtras();
