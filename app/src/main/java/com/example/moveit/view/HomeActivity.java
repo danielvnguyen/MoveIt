71dae20d30455676b16918de9f700d3b27f4d2ee
Daniel Nguyen <danielnguyen604@gmail.com>
2023-01-02 19:46:12 -0800

    setup daily reminder base func.

--- app/src/main/java/com/example/moveit/view/HomeActivity.java
+++ app/src/main/java/com/example/moveit/view/HomeActivity.java
@@ -2,14 +2,16 @@ package com.example.moveit.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.example.moveit.R;
+import com.example.moveit.alarm.NotificationReceiver;
 import com.example.moveit.model.ViewPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
-
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
@@ -52,6 +54,27 @@ public class HomeActivity extends AppCompatActivity {
                 Objects.requireNonNull(tabLayout.getTabAt(position)).select();
             }
         });
+
+        setUpAlarm();
+    }
+
+    private void setUpAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 17);
+        calendar.set(Calendar.MINUTE, 36);
+        calendar.set(Calendar.SECOND, 0);
+
+        if (calendar.getTime().compareTo(new Date()) < 0) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        if (alarmManager != null) {
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        }
     }
 
     @Override
