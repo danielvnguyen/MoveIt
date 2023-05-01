5f7c1c9e53b6d6541e88fc94f27d1a539c406f07
Daniel Nguyen <danielnguyen604@gmail.com>
2023-04-30 22:24:55 -0700

    add base calendar

--- app/src/main/java/com/example/moveit/view/fragments/CalendarPage.java
+++ app/src/main/java/com/example/moveit/view/fragments/CalendarPage.java
@@ -2,11 +2,14 @@ package com.example.moveit.view.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import com.example.moveit.R;
 
@@ -16,5 +19,15 @@ public class CalendarPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_calendar_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUpCalendar();
+    }
+
+    private void setUpCalendar() {
+        CalendarView calendarView = requireView().findViewById(R.id.calendarView);
     }
 }
