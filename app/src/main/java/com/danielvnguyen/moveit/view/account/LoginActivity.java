d7240b45415c1ab30f562b00123048fa9a116990
Daniel Nguyen <danielnguyen604@gmail.com>
2023-11-17 12:52:45 -0800

    move google sign-in to start activity

--- app/src/main/java/com/danielvnguyen/moveit/view/account/LoginActivity.java
+++ app/src/main/java/com/danielvnguyen/moveit/view/account/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.danielvnguyen.moveit.view.account;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Context;
@@ -10,32 +9,16 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.danielvnguyen.moveit.R;
-import com.danielvnguyen.moveit.model.activities.CategoryActivity;
-import com.danielvnguyen.moveit.model.categories.Category;
-import com.danielvnguyen.moveit.model.meals.Meal;
-import com.danielvnguyen.moveit.model.meals.ServingSize;
 import com.danielvnguyen.moveit.model.theme.ThemeSharedPreferences;
 import com.danielvnguyen.moveit.view.homepage.HomeActivity;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Objects;
-import java.util.UUID;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -43,32 +26,6 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordInput;
     private Button loginBtn;
     private FirebaseAuth auth;
-    private FirebaseFirestore db;
-
-    private GoogleSignInClient signInClient;
-    private static final int RC_SIGN_IN = 9001;
-    private static final String ID_TOKEN = "446715183529-ucspush1pj4sqs89s71ipeeoooq476e5.apps.googleusercontent.com";
-
-    private final String[] defaultCategories = {"Strength Exercises", "Cardio Exercises", "Flexibility Exercises", "Balance Exercises"};
-    private final String[][] defaultActivities = {{"Weightlifting", "Pull-ups", "Push-ups", "Planks", "Sit-ups"},
-            {"Running", "Walking", "Swimming", "Skip Rope", "Cycling"}, {"Yoga", "Meditation", "Stretching"},
-            {"High Knees", "Heel Taps", "Leg Raises", "Balance Walking"}};
-    private final Meal[] defaultMeals = {
-            new Meal(UUID.randomUUID().toString(), "Chicken Breast", 231,
-                    new ServingSize(140, "g"), "1 cup, chopped or diced",
-                    "https://cdn.pixabay.com/photo/2022/01/18/08/42/chicken-6946606_960_720.jpg"),
-            new Meal(UUID.randomUUID().toString(), "White Rice", 130,
-                    new ServingSize(100, "g"), "Cooked, long-grain",
-                    "https://cdn.pixabay.com/photo/2012/11/27/03/35/usd-67411_960_720.jpg"),
-            new Meal(UUID.randomUUID().toString(), "Chicken Caesar Salad", 309,
-                    new ServingSize(192,"g"), "Includes 6.67g crouton, 5.56g parmesan",
-                    "https://cdn.pixabay.com/photo/2014/01/17/08/56/caesar-246818_960_720.jpg"),
-            new Meal(UUID.randomUUID().toString(), "California Roll", 28,
-                    new ServingSize(30, "g"), "Non-spicy version",
-                    "https://cdn.pixabay.com/photo/2021/02/25/20/45/california-shrimp-roll-6050079_960_720.jpg"),
-            new Meal(UUID.randomUUID().toString(), "Quarter Pounder", 530,
-                    new ServingSize(220, "g"), "One hamburger with single patty and no condiments",
-                    "https://cdn.pixabay.com/photo/2017/04/04/16/25/hamburger-2201748_960_720.jpg")};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +35,6 @@ public class LoginActivity extends AppCompatActivity {
         setTitle(getString(R.string.login_title));
 
         auth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
 
         emailInput = findViewById(R.id.email);
         passwordInput = findViewById(R.id.password);
@@ -87,7 +43,6 @@ public class LoginActivity extends AppCompatActivity {
         setUpShowHideBtn();
         setUpLoginBtn();
         setUpForgotPasswordBtn();
-        setUpGoogleSignInBtn();
     }
 
     private void setUpTheme() {
@@ -114,53 +69,6 @@ public class LoginActivity extends AppCompatActivity {
                         break;
                 }
             }
-        }
-    }
-
-    private void setUpGoogleSignInBtn() {
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(ID_TOKEN)
-                .requestEmail()
-                .build();
-        signInClient = GoogleSignIn.getClient(this, signInOptions);
-
-        SignInButton googleSignInBtn = findViewById(R.id.googleSignInBtn);
-        googleSignInBtn.setOnClickListener(view -> {
-            Intent intent = signInClient.getSignInIntent();
-            startActivityForResult(intent, RC_SIGN_IN);
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleGoogleSignIn(accountTask);
-        }
-    }
-
-    private void handleGoogleSignIn(Task<GoogleSignInAccount> accountTask) {
-        try {
-            GoogleSignInAccount account = accountTask.getResult(ApiException.class);
-            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-            auth.signInWithCredential(credential).addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    if (Objects.requireNonNull(task.getResult().getAdditionalUserInfo()).isNewUser()) {
-                        //Initialize user only if this is their first time signing in
-                        initializeUser(Objects.requireNonNull(auth.getCurrentUser()).getUid());
-                    }
-                    setUpTheme();
-                    Toast.makeText(LoginActivity.this, "Log in successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                    finish();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Failed to log in", Toast.LENGTH_SHORT).show();
-                }
-            });
-        } catch (ApiException e) {
-            Log.d("LoginActivity", "Failed to log in", e);
         }
     }
 
@@ -228,39 +136,6 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
         overridePendingTransition(R.anim.nav_default_enter_anim, R.anim.nav_default_exit_anim);
-    }
-
-    private void initializeUser(String userUid) {
-        createDefaultCategories(userUid);
-        createDefaultMeals(userUid);
-    }
-
-    private void createDefaultMeals(String userUid) {
-        for (Meal currentMeal : defaultMeals) {
-            db.collection("meals").document(userUid).collection("mealList")
-                    .document(currentMeal.getId()).set(currentMeal);
-        }
-    }
-
-    private void createDefaultCategories(String userUid) {
-        for (int i = 0; i < defaultCategories.length; i++) {
-            int index = i;
-            String categoryId = UUID.randomUUID().toString();
-            Category newCategory = new Category(defaultCategories[i], categoryId);
-            db.collection("categories").document(userUid).collection("categoryList")
-                    .document(categoryId).set(newCategory).addOnSuccessListener(unused -> createDefaultActivities(index, categoryId, userUid));
-        }
-    }
-
-    private void createDefaultActivities(int position, String categoryId, String userUid) {
-        String[] currentActivities = defaultActivities[position];
-        for (String currentActivity : currentActivities) {
-            String activityId = UUID.randomUUID().toString();
-            CategoryActivity newActivity = new CategoryActivity(currentActivity, categoryId, activityId, "");
-            db.collection("categories").document(userUid)
-                    .collection("categoryList").document(categoryId)
-                    .collection("activityList").document(activityId).set(newActivity);
-        }
     }
 
     @Override
